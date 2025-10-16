946896c3edaafe517770e8fd8b3234cc5c176a97
eppalapallysharath <eppalapallysharath@gmail.com>
2025-10-16 16:13:24 +0530

    components

---
+++ react_components/src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header =()=>{
+    return<header>
+        <p>myntra</p>
+        <img src="https://static-assets-web.flixcart.com/batman-returns/batman-returns/p/images/fkheaderlogo_exploreplus-44005d.svg" />
+    </header>
+}
+
+export default Header;
---
+++ react_components/src/components/class components/cards.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Cards extends React.Component{
+    render(){
+        return<div>
+            <img src="https://rukminim2.flixcart.com/image/240/240/xif0q/mobile/y/w/k/-original-imahfvpcn3j8qwzk.jpeg?q=60" alt="https://rukminim2.flixcart.com/image/240/240/xif0q/mobile/y/w/k/-original-imahfvpcn3j8qwzk.jpeg?q=60"/>
+            <p>mobile phone</p>
+        </div>
+    }
+}
+
+export default Cards
